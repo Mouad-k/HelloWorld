58b93425ed36ab470c20dd7231036de18174c576
Mouad-k <165574651+Mouad-k@users.noreply.github.com>
2024-04-06 18:33:09 +0000

    new changes (#2)
    * new changes
    
    * clean code

--- src/main/java/com/project/HelloWorld/ServicesImp/HelloServiceImp.java
+++ src/main/java/com/project/HelloWorld/ServicesImp/HelloServiceImp.java
@@ -1,15 +1,28 @@
 package com.project.HelloWorld.ServicesImp;
 
+import com.project.HelloWorld.Entities.HelloEntity;
+import com.project.HelloWorld.Repositories.HelloRepository;
 import com.project.HelloWorld.Services.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class HelloServiceImp implements HelloService {
-    @Override
-    public String getHelloMessage() {
-        return "Hello world";
+
+    private final HelloRepository helloRepository;
+
+
+    public HelloServiceImp(HelloRepository helloRepository) {
+        this.helloRepository = helloRepository;
     }
 
-
-
+    @Override
+    public String getHelloMessage() {
+        HelloEntity helloEntity = helloRepository.findById(1L).orElse(null);
+        if (helloEntity != null) {
+            return helloEntity.getMessage();
+        } else {
+            return "error";
+        }
+    }
 }
--- src/main/java/com/project/HelloWorld/ServicesImp/HelloServiceImpTest.java
+++ src/main/java/com/project/HelloWorld/ServicesImp/HelloServiceImpTest.java
@@ -1,27 +1,51 @@
 package com.project.HelloWorld.ServicesImp;
+
+import com.project.HelloWorld.Entities.HelloEntity;
+import com.project.HelloWorld.Repositories.HelloRepository;
 import com.project.HelloWorld.Services.HelloService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.when;
 
-
-@SpringBootTest
 public class HelloServiceImpTest {
-    @Autowired
-   private HelloServiceImp HelloService ;
 
-    @Test
-    public void GetHelloMessageTest(){
-        String message = HelloService.getHelloMessage();
-        assertNotNull(message);
-        assertEquals("Hello world" , message);
+    @Mock
+    private HelloRepository helloRepository;
 
+    private HelloService helloService;
 
-
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        helloService = new HelloServiceImp(helloRepository);
     }
 
+    @Test
+    public void testGetHelloMessage() {
 
+        HelloEntity mockHelloEntity = new HelloEntity();
+        mockHelloEntity.setMessage("Hello world");
+        when(helloRepository.findById(1L)).thenReturn(Optional.of(mockHelloEntity));
 
+        String helloMessage = helloService.getHelloMessage();
+
+        assertEquals("Hello world", helloMessage);
+    }
+
+    @Test
+    public void testGetHelloMessageWhenNotFound() {
+
+        when(helloRepository.findById(1L)).thenReturn(Optional.empty());
+
+        String helloMessage = helloService.getHelloMessage();
+
+        assertEquals("error", helloMessage);
+    }
 }
